7210533f4486fdcf42d950c0f0bf8edcd1e1b7b2
ccmatrix501-spec <ccmatrix501@gmail.com>
2026-08-30 05:09:09 +1000

    Assemble HooknGaffe portrait at build time

---
+++ src/data/hookngaffe-portrait-v2.ts
@@ -0,0 +1,8 @@
+import { part1 } from "./hookngaffe-v2/part1";
+import { part2 } from "./hookngaffe-v2/part2";
+import { part3 } from "./hookngaffe-v2/part3";
+import { part4 } from "./hookngaffe-v2/part4";
+import { part5 } from "./hookngaffe-v2/part5";
+
+export const hooknGaffePortraitV2 =
+  `data:image/jpeg;base64,${part1}${part2}${part3}${part4}${part5}`;
