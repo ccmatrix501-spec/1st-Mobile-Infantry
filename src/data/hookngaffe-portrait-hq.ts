dd7caddb454a0e3260801e8f86a031fc095090be
ccmatrix501-spec <ccmatrix501@gmail.com>
2026-08-30 05:38:07 +1000

    Assemble high quality HooknGaffe AVIF portrait

---
+++ src/data/hookngaffe-portrait-hq.ts
@@ -0,0 +1,14 @@
+import { part1 } from "./hookngaffe-hq/part1";
+import { part2 } from "./hookngaffe-hq/part2";
+import { part3 } from "./hookngaffe-hq/part3";
+import { part4 } from "./hookngaffe-hq/part4";
+import { part5 } from "./hookngaffe-hq/part5";
+import { part6 } from "./hookngaffe-hq/part6";
+import { part7 } from "./hookngaffe-hq/part7";
+import { part8 } from "./hookngaffe-hq/part8";
+import { part9 } from "./hookngaffe-hq/part9";
+import { part10 } from "./hookngaffe-hq/part10";
+import { part11 } from "./hookngaffe-hq/part11";
+
+export const hooknGaffePortraitHQ =
+  `data:image/avif;base64,${part1}${part2}${part3}${part4}${part5}${part6}${part7}${part8}${part9}${part10}${part11}`;
